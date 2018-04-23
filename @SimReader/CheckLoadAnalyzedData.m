419e0a9704029866763e9fc8f51b0845d320a7e7
Filippo Vicentini <filippovicentini@gmail.com>
2018-04-23 12:27:21 +0200

    Refactored analysis/skipanalysis logic

--- @SimReader/CheckLoadAnalyzedData.m
+++ @SimReader/CheckLoadAnalyzedData.m
@@ -1,43 +1,95 @@
 function loaded = CheckLoadAnalyzedData( obj )
-%CHECKLOADANALYZEDDATA Reads and Analizes data, after the path has been correctly 
+%CHECKLOADANALYZEDDATA Reads and Analizes data, after the path has been correctly
 %set up in the class instance.
 %   Detailed explanation goes here
-    
+
     dataPath = fullfile(obj.simPath, obj.simDataFolderName);
     anPath = obj.averagedPath;
     aveDataPath = fullfile(anPath, obj.fileAveragedFileName);
     quanDataPath = fullfile(anPath, obj.fileQuantitiesFileName);
-    
 
     loaded = false;
 
     if ((obj.IsDataUpToDate(aveDataPath, dataPath) && ~obj.forceAnalysis) || obj.ForceNoAnalysis)
-      fprintf(['Averaged and Quantities data is up', ...
+        fprintf(['Averaged and Quantities data is up', ...
             ' to date. Loading .mat file.\n']);
-      aveData = load(aveDataPath);
-       
-      if (~obj.ForceNoAnalysis)
-        if isfield(aveData.params, 'VERSION') 
-          if (aveData.params.VERSION < obj.VERSION)
-            fprintf('Ah, no. Analysis VERSION has been bumped. Forcing reanalysis.\n');
-            obj.forceAnalysis = true;
-            return
-          end
-        else
-          if (obj.VERSION ~= 0)
-            fprintf('Ah, no. Analysis VERSION has been bumped. Forcing reanalysis.\n');
-            obj.forceAnalysis = true;
-            return;
+        aveData = load(aveDataPath);
+        
+        if (~obj.ForceNoAnalysis)
+            if isfield(aveData.params, 'VERSION')
+                if (aveData.params.VERSION < obj.VERSION)
+                    fprintf('Ah, no. Analysis VERSION has been bumped. Forcing reanalysis.\n');
+                    obj.forceAnalysis = true;
+                    return
+                end
+            else
+                if (obj.VERSION ~= 0)
+                    fprintf('Ah, no. Analysis VERSION has been bumped. Forcing reanalysis.\n');
+                    obj.forceAnalysis = true;
+                    return;
+                end
+            end
+            
+            if obj.analysisParameters.active == true
+                if isfield(aveData.params, 'analysisParameters')
+                    if ~isequaln(aveData.params.analysisParameters, obj.analysisParameters)
+                        fprintf('Ah, no. Asking special Additional Analysis.\n');
+                        obj.forceAnalysis = true;
+                        return
+                    end
+                else
+                    fprintf('Ah, no. Asking special Additional Analysis.\n');
+                    obj.forceAnalysis = true;
+                    return
+                end
+                fprintf('Special Analysis with those parameters alredy performed.\n');
+            end
         end
-      end
+        
+        obj.ave = aveData.averaged;
+        obj.params = aveData.params;
+        clear aveData;
+        quanData = load(quanDataPath);
+        obj.quan = quanData.quantities;
+        clear quanData;
+        
+        loaded = true;
+    else
+        if exist(aveDataPath)
+            aveData = load(aveDataPath);
+            if (~obj.ForceNoAnalysis)
+                if isfield(aveData.params, 'VERSION')
+                    if (aveData.params.VERSION < obj.VERSION)
+                        fprintf('Ah, no. Analysis VERSION has been bumped. Forcing reanalysis.\n');
+                        obj.forceAnalysis = true;
+                        return
+                    end
+                else
+                    if (obj.VERSION ~= 0)
+                        fprintf('Ah, no. Analysis VERSION has been bumped. Forcing reanalysis.\n');
+                        obj.forceAnalysis = true;
+                        return;
+                    end
+                end
+            end
+            
+            if obj.analysisParameters.active == true
+                if isfield(aveData.params, 'analysisParameters')
+                    if ~isequaln(aveData.params.analysisParameters, obj.analysisParameters)
+                        fprintf('Ah, no. Asking special Additional Analysis.\n');
+                        obj.forceAnalysis = true;
+                        return
+                    end
+                else
+                    fprintf('Ah, no. Asking special Additional Analysis.\n');
+                    obj.forceAnalysis = true;
+                    return
+                end
+                fprintf('Special Analysis with those parameters alredy performed.\n');
+            end
 
-      obj.ave = aveData.averaged;
-      obj.params = aveData.params;
-      clear aveData;
-      quanData = load(quanDataPath);
-      obj.quan = quanData.quantities;
-      clear quanData;
-
-      loaded = true;
+        else
+            obj.forceAnalysis = true;
+        end
     end
 end
