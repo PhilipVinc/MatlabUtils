e3441c5cb200891d222d5161d1e26c9d6446e296
Filippo Vicentini <filippovicentini@gmail.com>
2018-04-23 12:27:01 +0200

    TOML Support new INI support

--- @SimReader/ReadIniFile.m
+++ @SimReader/ReadIniFile.m
@@ -2,59 +2,123 @@ function  output = ReadIniFile(obj)
 %READINIFILE Summary of this function goes here
 %   Detailed explanation goes here
     
-    % Find ini File:
-    iniFiles = dir(fullfile(obj.simPath,'*.ini'));
+    tomlFile = dir(fullfile(obj.simPath,'*.toml'));
     
-    if (~length(iniFiles) == 1)
-        output = ['Error: Found', num2str(length(iniFiles)),'ini Files. 1 Expected.'];
-        fprintf(output);
-        return;
-    end
-    
-    iniFilePath = fullfile(obj.simPath, iniFiles(1).name);
-    fprintf(['Loading ini file: ', iniFilePath,'\n']);    
-    
-    % Read the ini file with ini2struct.m
-    params = ini2struct(iniFilePath);
-    fields = fieldnames(params);
-    
-    % Convert string with numbers to numbers; save the rest as strings
-    for ii=1:length(fields)
-        field = fields{ii};
-        tmp = params.(field);
-        [val, success] = str2num(tmp);
-        if success
-            params.(field) = val;
-        elseif strcmp(tmp(end-3:end), '.dat')
-            dataFilePath = fullfile(obj.simPath, tmp);
-            
-            success = false;
-            if exist(dataFilePath, 'file')
-                try
-                    varData = load(dataFilePath);
-                    params.(field) = varData;
-                    success = true;
-                catch exception
-                end
-            end
-            if ~success
-                params.(field) = tmp;
-                fprintf(['INI: Could not find file ', dataFilePath, ...
-                    ' for key ', field]);
-            end
-        else
-            params.(field) = tmp;
-        end
-    end
-    
-    % Check if the local basis size is specified
-    if ~isfield(obj.params,'local_cell_size')
-        params.local_cell_size=1;
-    end
-    
-    % Store the results
-    obj.params = params;
+    if (length(tomlFile) == 1)
+        iniFiles = tomlFile;
 
-    output = 'success';
+        if (~length(iniFiles) == 1)
+            output = ['Error: Found', num2str(length(iniFiles)),'ini Files. 1 Expected.'];
+            fprintf(output);
+            return;
+        end
+
+        iniFilePath = fullfile(obj.simPath, iniFiles(1).name);
+        fprintf(['Loading ini file: ', iniFilePath,'\n']);    
+
+        % Read the ini file with ini2struct.m
+        params = toml2struct(iniFilePath);
+        fields = fieldnames(params);
+
+        % Convert string with numbers to numbers; save the rest as strings
+        for ii=1:length(fields)
+            field = fields{ii};
+            tmp = params.(field);
+            
+            if isstruct(tmp) 
+                if isfield(tmp, 'file')
+                    dataFilePath = fullfile(obj.simPath, tmp.file);
+                    try
+                        varData = load(dataFilePath);
+                        params.(field) = varData;
+                        success = true;
+                        if isfield(tmp, 'complex')
+                            if tmp.complex == true
+                                tmp = varData(1:2:end)+1j*varData(2:2:end);
+                                params.(field) = tmp;
+                            end
+                        end
+                    catch exception
+                    end
+
+                elseif isfield (tmp, 'value')
+                    params.(field) = tmp.value;
+                elseif isfield (tmp, 'real')
+                    params.(field) = tmp.real;
+                    if isfield(tmp, 'imag')
+                        params.(field)  = params.(field) +1j*tmp.imag;
+                    end
+                end
+            else
+                params.(field) = tmp;
+            end
+        end
+
+        % Check if the local basis size is specified
+        if ~isfield(obj.params,'local_cell_size')
+            params.local_cell_size=1;
+        end
+
+        % Store the results
+        obj.params = params;
+
+        output = 'success';
+
+    else
+        % Find ini File:
+        iniFiles = dir(fullfile(obj.simPath,'*.ini'));
+
+        if (~length(iniFiles) == 1)
+            output = ['Error: Found', num2str(length(iniFiles)),'ini Files. 1 Expected.'];
+            fprintf(output);
+            return;
+        end
+
+        iniFilePath = fullfile(obj.simPath, iniFiles(1).name);
+        fprintf(['Loading ini file: ', iniFilePath,'\n']);    
+
+        % Read the ini file with ini2struct.m
+        params = ini2struct(iniFilePath);
+        fields = fieldnames(params);
+
+        % Convert string with numbers to numbers; save the rest as strings
+        for ii=1:length(fields)
+            field = fields{ii};
+            tmp = params.(field);
+            [val, success] = str2num(tmp);
+            if success
+                params.(field) = val;
+            elseif strcmp(tmp(end-3:end), '.dat')
+                dataFilePath = fullfile(obj.simPath, tmp);
+
+                success = false;
+                if exist(dataFilePath, 'file')
+                    try
+                        varData = load(dataFilePath);
+                        params.(field) = varData;
+                        success = true;
+                    catch exception
+                    end
+                end
+                if ~success
+                    params.(field) = tmp;
+                    fprintf(['INI: Could not find file ', dataFilePath, ...
+                        ' for key ', field], '\n');
+                end
+            else
+                params.(field) = tmp;
+            end
+        end
+
+        % Check if the local basis size is specified
+        if ~isfield(obj.params,'local_cell_size')
+            params.local_cell_size=1;
+        end
+
+        % Store the results
+        obj.params = params;
+
+        output = 'success';
+    end
 end
 
